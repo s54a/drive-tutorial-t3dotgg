cd7318c5d984258ef2e2d431dc932dcdea9207a2
Sooraj Gupta <soorajgupta00@gmail.com>
2025-10-15 01:38:17 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center text-7xl uppercase dark:bg-black dark:text-white">
+      drive tutorial
+    </div>
+  );
+};
+
+export default page;
